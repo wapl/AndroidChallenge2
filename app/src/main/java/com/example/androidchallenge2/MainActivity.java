b740f45f3cd28c2c07027eeadfdfc451f40084c6
wapl <33437769+wapl@users.noreply.github.com>
2019-11-22 19:39:54 +0800

    FROM THE BEGGNiNG

--- app/src/main/java/com/example/androidchallenge2/MainActivity.java
+++ app/src/main/java/com/example/androidchallenge2/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.androidchallenge2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.recycler_area);
+
     }
 }
